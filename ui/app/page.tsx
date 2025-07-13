97daccffcac0ff2178f4af3aba50b00b0a8f32a6
AjayiMike <ajayimikeofficial@gmail.com>
2025-07-13 03:53:48 +0100

    chore: update dependencies in pnpm-lock.yaml and add package-lock.json for UI

--- ui/app/page.tsx
+++ ui/app/page.tsx
@@ -5,28 +5,28 @@ import { DisperseTokens } from "@/components/DisperseTokens";
 import { BurnTokens } from "@/components/BurnTokens";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-4 md:p-8 lg:p-12">
-      <Tabs defaultValue="mint" className="w-full max-w-lg">
-        <TabsList className="grid w-full grid-cols-4">
-          <TabsTrigger value="mint">Mint</TabsTrigger>
-          <TabsTrigger value="transfer">Transfer</TabsTrigger>
-          <TabsTrigger value="disperse">Disperse</TabsTrigger>
-          <TabsTrigger value="burn">Burn</TabsTrigger>
-        </TabsList>
-        <TabsContent value="mint">
-          <MintTokens />
-        </TabsContent>
-        <TabsContent value="transfer">
-          <TransferTokens />
-        </TabsContent>
-        <TabsContent value="disperse">
-          <DisperseTokens />
-        </TabsContent>
-        <TabsContent value="burn">
-          <BurnTokens />
-        </TabsContent>
-      </Tabs>
-    </main>
-  );
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center p-4 md:p-8 lg:p-12">
+            <Tabs defaultValue="mint" className="w-full max-w-lg">
+                <TabsList className="grid w-full grid-cols-4">
+                    <TabsTrigger value="mint">Mint</TabsTrigger>
+                    <TabsTrigger value="transfer">Transfer</TabsTrigger>
+                    <TabsTrigger value="disperse">Disperse</TabsTrigger>
+                    <TabsTrigger value="burn">Burn</TabsTrigger>
+                </TabsList>
+                <TabsContent value="mint">
+                    <MintTokens />
+                </TabsContent>
+                <TabsContent value="transfer">
+                    <TransferTokens />
+                </TabsContent>
+                <TabsContent value="disperse">
+                    <DisperseTokens />
+                </TabsContent>
+                <TabsContent value="burn">
+                    <BurnTokens />
+                </TabsContent>
+            </Tabs>
+        </main>
+    );
 }
